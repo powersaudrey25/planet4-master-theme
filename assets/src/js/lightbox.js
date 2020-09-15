b1fd19977c55f06ab8728ae35bf5a11b2fbbdd7e
Pablo Cubico <pablocuadrado@gmail.com>
2020-09-15 20:52:59 -0300

    Adding indicators between image and caption

--- assets/src/js/lightbox.js
+++ assets/src/js/lightbox.js
@@ -2,6 +2,36 @@ const GRID_GALLERY_CLASS = 'gallery-grid';
 const SLIDER_GALLERY_CLASS = 'carousel-wrap';
 const THREE_COLUMN_GALLERY_CLASS = 'three-column-images';
 
+const renderIndicators = (gallery) => {
+  const indicatorSpans = document.querySelectorAll('.p4-photoswipe-indicators-wrapper span');
+  const indicatorsWrapper = document.querySelector('.p4-photoswipe-indicators-wrapper');
+
+  const topGap = gallery.currItem.bounds ? gallery.currItem.bounds.center.y : 0;
+  let currentItemBottomBorder = (gallery.currItem.h * gallery.currItem.fitRatio) + topGap;
+  indicatorsWrapper.style.top = currentItemBottomBorder + 'px';
+
+  if (indicatorSpans.length !== gallery.options.getNumItemsFn()) {
+    indicatorsWrapper.innerHTML = '';
+    for (let i = 0; i < gallery.options.getNumItemsFn(); i++) {
+      let indicatorClickArea = document.createElement('span');
+      indicatorClickArea.classList.add('p4-photoswipe-indicator-click-area')
+      let indicatorBar = document.createElement('span');
+      indicatorBar.classList.add('p4-photoswipe-indicator-bar')
+      indicatorClickArea.appendChild(indicatorBar);
+      indicatorClickArea.addEventListener('click', e => {
+        e.preventDefault();
+        gallery.goTo(i);
+      });
+      indicatorsWrapper.appendChild(indicatorClickArea);
+    }
+  }
+
+  indicatorSpans.forEach(item => item.classList.remove(['active']));
+
+  const currentIndicator = document.querySelector(`.p4-photoswipe-indicators-wrapper > span:nth-child(${gallery.getCurrentIndex() + 1})`);
+  currentIndicator.classList.add(['active']);
+}
+
 const setupPhotoSwipe = function(items, clickedIndex) {
   var pswpElement = $('.pswp')[0];
 
@@ -10,6 +40,7 @@ const setupPhotoSwipe = function(items, clickedIndex) {
     fullscreenEl: false,
     zoomEl: false,
     index: clickedIndex,
+    closeOnScroll: false,
   };
 
   var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
@@ -24,6 +55,19 @@ const setupPhotoSwipe = function(items, clickedIndex) {
       imageSizeHandler.src = galleryItem.src;
     }
   });
+
+  gallery.listen('afterChange', () => {
+    renderIndicators(gallery);
+  });
+
+  gallery.listen('resize', () => {
+    renderIndicators(gallery);
+  });
+
+  window.addEventListener("resize", () => {
+    renderIndicators(gallery);
+  });
+
   gallery.init();
 }
 
